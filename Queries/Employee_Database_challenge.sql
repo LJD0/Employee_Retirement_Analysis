dc76bd7514a0f80001ccdcd0ea311200823b1101
OAO-MadHatter <larsenj2064@gmail.com>
2022-10-19 19:04:56 -0700

    Final Challenge Commit no README

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -27,3 +27,14 @@ ORDER BY COUNT((emp_no)) DESC;
 
 -- Deliverable 2
 
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+-- INTO mentorship_ability
+FROM employees as e
+    INNER JOIN dept_emp as de
+        ON (e.emp_no = de.emp_no)
+            INNER JOIN titles as t
+                ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+    AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
+
